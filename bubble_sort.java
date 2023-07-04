1be6e035ea95ba4200c91be44f8f94c38918f8c0
Debasis <debasisjena693@gmail.com>
2023-07-04 18:20:55 +0530

    Updated bubble_sort.java with modifications

--- bubble_sort.java
+++ bubble_sort.java
@@ -1,22 +1,23 @@
 public class bubble_sort {
-    public static void printArray(int arr[]){
-            for(int i=0;i<arr.length;i++){
-                System.out.print(arr[i]+" ");
-            }
-            System.out.println();
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
-    public static void main(String[] args){
-        int arr[] = {10,5,6,12,8};
-        //time complexity = O^2
+
+    public static void main(String[] args) {
+        int arr[] = { 10, 5, 6, 12, 8 };
+        // time complexity = O^2
         // bubble sort
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=0;j<arr.length-i-1;j++){
-                if(arr[j]>arr[j+1]){
-                    //swap
-                    int temp = arr[j]; //assign arr[j] value to a temporary variable
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                    
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j]; // assign arr[j] value to a temporary variable
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
                 }
             }
         }
